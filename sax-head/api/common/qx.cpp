e702a44d7dc2aa8a0180e2c1c6e1f85d3e69cf24
ms <ms@a26604f9-b0b9-0310-ba11-92d0f7005c15>
2003-09-18 07:20:52 +0000

    - fixed memory leak in va_arg handling

--- sax-head/api/common/qx.cpp
+++ sax-head/api/common/qx.cpp
@@ -34,103 +34,104 @@ STATUS        : development
 char* qx (char*command,int channel,int anz,char* format,...) {
 	char data[30] = "/tmp/data";
 	char *result  = NULL;
+	FILE* new_channel = NULL;
 	char *arg[anz+2];
 	va_list ap;
-	FILE* new_channel = NULL;
 	FILE *fd;
 	int n=1;
 	long size=0;
 	int child;
 
 	signal( SIGCHLD , handlesigchld );
-
-	//=================================
-	// prepare arguments for execv...
-	//---------------------------------
-	arg[0] = (char*)malloc(sizeof(char)*MAX_PROGRAM_SIZE);
-	strcpy(arg[0],basename (command));
-	arg[anz+1] = NULL;
-
-	// prepare data file...
-	// ---------------------
 	sprintf(data,"%s-%d",data,getpid());
 
-	// get arguments...
-	// ----------------- 
-	if (format != NULL) {
-		va_start(ap, format);
-		while (*format) {
-			switch(*format++) {
-			case 's':
-			arg[n] = strdup(va_arg(ap, char*));
-			n++;
-			break;
-			}
-		}
-		va_end(ap);
-	}
-	arg[n] = NULL;
-
 	//=================================
-	// call the program...
+	// call the program (prefork)...
 	//---------------------------------
 	switch ( child = fork() ) {
+
+	//=================================
+	// Check system call...
+	//---------------------------------
 	case -1:
-	log(L_ERROR,
-		"qx: could not fork: %s\n",strerror(errno)
-	);
-	exit(1);
-
-	case 0:
-	// child process...
-	// -----------------
-	if (channel == 0) {
-		new_channel = freopen(data,"w",stdout);
-	}
-	if (channel == 1) {
-		new_channel = freopen(data,"w",stderr);
-	}
-	execv (command,arg);
-
-	// ups should not happen here...
-	// ------------------------------
-	if (new_channel) {
-		fclose (new_channel);
-		unlink (data);
-	}
-	log(L_ERROR,
-		"qx: could not execute command: %s\n",command
-	);
-	exit (0);
-
-	default:
-	// parent process...
-	// ------------------
-	waitpid (child,NULL,0);
-	if ((channel == 0) || (channel == 1)) {
-	while (getproc(data)) {
-		usleep(500);
-	}
-	if (! (fd = fopen(data,"r"))) {
-		return(NULL);
-	}
-	fseek(fd,0L,SEEK_END);
-	size = ftell(fd) - 1;
-	rewind(fd);
-	if (size <= 0) {
-		fclose(fd); unlink(data); 
-		log(L_INFO,
-        	"qx: command returned (null) string\n"
+		log(L_ERROR,
+			"qx: could not fork: %s\n",strerror(errno)
 		);
-		return(NULL);
-	}
-	result = (char*)malloc(sizeof(char)*size + 1);
-	fread(result,size,1,fd);
-	result[size] = '\0';
-	fclose(fd);
-	unlink(data);
-	}
-	return(result);
+		exit(1);
+	break;
+
+	//=================================
+	// Handle child process...
+	//---------------------------------
+	case 0:
+		// ... /
+		// prepare arguments for execv...
+		// ---
+		arg[0] = (char*)malloc(sizeof(char)*MAX_PROGRAM_SIZE);
+		strcpy(arg[0],basename (command));
+		arg[anz+1] = NULL;
+		if (format != NULL) {
+			va_start(ap, format);
+			while (*format) {
+				switch(*format++) {
+				case 's':
+				arg[n] = strdup(va_arg(ap, char*));
+				n++;
+				break;
+				}
+			}
+			va_end(ap);
+		}
+		arg[n] = NULL;
+		if (channel == 0) {
+			new_channel = freopen(data,"w",stdout);
+		}
+		if (channel == 1) {
+			new_channel = freopen(data,"w",stderr);
+		}
+		execv (command,arg);
+		// ... /
+		// the following code shouldn't be reached
+		// ---
+		if (new_channel) {
+			fclose (new_channel);
+			unlink (data);
+		}
+		log(L_ERROR,
+			"qx: could not execute command: %s\n",command
+		);
+		exit (0);
+	break;
+
+	//=================================
+	// Handle parent process...
+	//---------------------------------
+	default:
+		waitpid (child,NULL,0);
+		if ((channel == 0) || (channel == 1)) {
+		while (getproc(data)) {
+			usleep(500);
+		}
+		if (! (fd = fopen(data,"r"))) {
+			return(NULL);
+		}
+		fseek(fd,0L,SEEK_END);
+		size = ftell(fd) - 1;
+		rewind(fd);
+		if (size <= 0) {
+			fclose(fd); unlink(data); 
+			log(L_INFO,
+    	    	"qx: command returned (null) string\n"
+			);
+			return(NULL);
+		}
+		result = (char*)malloc(sizeof(char)*size + 1);
+		fread(result,size,1,fd);
+		result[size] = '\0';
+		fclose(fd);
+		unlink(data);
+		}
+		return(result);
 	}
 }
 
