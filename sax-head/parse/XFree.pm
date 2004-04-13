61125fae1ce730ef885c1e172d1542e1a693c2b2
ms <ms@a26604f9-b0b9-0310-ba11-92d0f7005c15>
2004-04-13 11:13:20 +0000

    - adapt time stamp

--- sax-head/parse/XFree.pm
+++ sax-head/parse/XFree.pm
@@ -3,10 +3,65 @@ package XFree;
 require Exporter;
 require DynaLoader;
 @ISA = qw(Exporter DynaLoader);
-package XFree;
+package XFreec;
 bootstrap XFree;
-var_XFree_init();
 package XFree;
 @EXPORT = qw( );
-1;
 
+# ---------- BASE METHODS -------------
+
+package XFree;
+
+sub TIEHASH {
+    my ($classname,$obj) = @_;
+    return bless $obj, $classname;
+}
+
+sub CLEAR { }
+
+sub FIRSTKEY { }
+
+sub NEXTKEY { }
+
+sub FETCH {
+    my ($self,$field) = @_;
+    my $member_func = "swig_${field}_get";
+    $self->$member_func();
+}
+
+sub STORE {
+    my ($self,$field,$newval) = @_;
+    my $member_func = "swig_${field}_set";
+    $self->$member_func($newval);
+}
+
+sub this {
+    my $ptr = shift;
+    return tied(%$ptr);
+}
+
+
+# ------- FUNCTION WRAPPERS --------
+
+package XFree;
+
+*ReadConfigFile = *XFreec::ReadConfigFile;
+*GetModulePath = *XFreec::GetModulePath;
+*GetFontPath = *XFreec::GetFontPath;
+*GetRgbPath = *XFreec::GetRgbPath;
+*GetLogFile = *XFreec::GetLogFile;
+*GetModuleSpecs = *XFreec::GetModuleSpecs;
+*GetModuleSubSpecs = *XFreec::GetModuleSubSpecs;
+*GetInputSpecs = *XFreec::GetInputSpecs;
+*GetFlags = *XFreec::GetFlags;
+*GetLayoutSpecs = *XFreec::GetLayoutSpecs;
+*GetDeviceSpecs = *XFreec::GetDeviceSpecs;
+*GetMonitorSpecs = *XFreec::GetMonitorSpecs;
+*GetModesSpecs = *XFreec::GetModesSpecs;
+*GetDisplaySpecs = *XFreec::GetDisplaySpecs;
+
+# ------- VARIABLE STUBS --------
+
+package XFree;
+
+1;
