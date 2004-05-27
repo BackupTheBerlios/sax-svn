4637d34627f57bbff51b93684b02f82d53362086
ms <ms@a26604f9-b0b9-0310-ba11-92d0f7005c15>
2004-05-27 08:51:16 +0000

    - fixed use of ininitialized structure variable (#38660)

--- sax-head/sysp/lib/hw/mouse.c
+++ sax-head/sysp/lib/hw/mouse.c
@@ -20,13 +20,13 @@ STATUS        : development
 // GetMouseData...
 //-----------------------------------
 MouseData* MouseGetData(void) {
-	hd_data_t *hd_data;
-	driver_info_t *di;
-	MouseData* data  = NULL;
-	MouseData* first = NULL;
-	MouseData* last  = NULL;
-	hd_t *hd         = NULL;
-	hd_t *first_dev;
+	hd_data_t *hd_data = NULL;
+	driver_info_t *di  = NULL;
+	MouseData* data    = NULL;
+	MouseData* first   = NULL;
+	MouseData* last    = NULL;
+	hd_t *hd           = NULL;
+	hd_t *first_dev    = NULL;
 
 	hd_data = (hd_data_t*)calloc(1, sizeof *hd_data);
 	hd_data->flags.fast = 1;
@@ -34,6 +34,7 @@ MouseData* MouseGetData(void) {
 	first_dev = hd;
 
 	for(; hd; hd = hd->next) {
+		//===================================
 		// create new element and defaults...
 		// -----------------------------------
 		data = (MouseData*)malloc(sizeof(MouseData));
@@ -51,9 +52,9 @@ MouseData* MouseGetData(void) {
 		if (hd == first_dev) {
 			first = data;
 		}
-
+		//===================================
 		// save hddata to struct...
-		// ---------------------------
+		// ----------------------------------
 		#if HD_VERSION >= 5
 		di = hd->driver_info;
 		#else
@@ -62,17 +63,31 @@ MouseData* MouseGetData(void) {
 		if(di && di->any.type == di_mouse && di->mouse.xf86) {
 			strcpy(data->protocol,di->mouse.xf86);
 			#ifdef WHEEL_SUPPORT
-			data->buttons = di->mouse.buttons;
-			data->wheel   = di->mouse.wheels;
-			if ((data->buttons >= 3)||(strcmp(data->protocol,"imps/2") == 0)) {
+			if (di->mouse.buttons) {
+				data->buttons = di->mouse.buttons;
+			}
+			if (di->mouse.wheels) {
+				data->wheel   = di->mouse.wheels;
+			}
+			if (data->buttons) {
+			if (
+				(data->buttons >= 3) || 
+				(strcmp(data->protocol,"imps/2") == 0) ||
+				(strcmp(data->protocol,"explorerps/2") == 0)
+			) {
 				data->emulate = 0;
 			}
+			}
+			if (data->wheel) {
 			if (data->wheel >= 1) {
 				data->buttons = (data->wheel * 2) + data->buttons;
 			}
+			}
 			#endif
 		}
-		strcpy(data->device,hd->unix_dev_name);
+		if (hd->unix_dev_name) {
+			strcpy(data->device,hd->unix_dev_name);
+		}
 		last = data;
 	}
 	return(first);
