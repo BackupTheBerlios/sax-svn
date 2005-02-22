383b3433f278c25e5367eb1100b92e7addc76492
ms <ms@a26604f9-b0b9-0310-ba11-92d0f7005c15>
2005-02-22 14:45:59 +0000

    - add install target

--- sax-head/libsax/bindings/csharp/test.cs
+++ sax-head/libsax/bindings/csharp/test.cs
@@ -2,29 +2,21 @@ using System;
 
 public class SaXTest {
 	static void Main() {
-		Console.WriteLine("Hello World");
+		SaXImport desktop = new SaXImport ( 5 ); // SaX.SAX_DESKTOP
+		SaXImport card    = new SaXImport ( 4 );
+		SaXImport path    = new SaXImport ( 9 );
 
-		using (SaXImport desktop = new SaXImport ( 5 )) // SaX.SAX_DESKTOP -> abort()
-		using (SaXImport card    = new SaXImport ( 4 ))
-		using (SaXImport path    = new SaXImport ( 9 ))
-		using (SaXInit init = new SaXInit())
-		{
-			Console.WriteLine("Created SaXInit " + init);
+		card.doImport();
+		desktop.doImport();
+		path.doImport();
 
-			card.doImport();
-			desktop.doImport();
-			path.doImport();
+		SaXManipulateDesktop manip = new SaXManipulateDesktop (
+			desktop,card,path,0
+		);
+		String name = manip.getMonitorName();
+		Console.WriteLine("Monitor name: " + name);
 
-			using ( SaXManipulateDesktop manip = new SaXManipulateDesktop (
-				desktop,card,path,0
-			))
-			{
-				String name = manip.getMonitorName();
-				Console.WriteLine("Monitor name: " + name);
-
-				//String[] lala = manip.getCDBMonitorList();
-				//Console.WriteLine("First element: " + lala[0]);
-			}
-		}
+		//String[] lala = manip.getCDBMonitorList();
+		//Console.WriteLine("First element: " + lala[0]);
 	}
 }
