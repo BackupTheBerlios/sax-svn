ca66b18b1b20db806500194248482163ad448946
ms <ms@a26604f9-b0b9-0310-ba11-92d0f7005c15>
2005-02-22 17:17:54 +0000

    - added doxygen data

--- sax-head/libsax/desktop.cpp
+++ sax-head/libsax/desktop.cpp
@@ -27,8 +27,8 @@ SaXManipulateDesktop::SaXManipulateDesktop (
 	SaXImport* desktop, SaXImport* card , SaXImport* path, int desktopID
 ) {
 	// .../
-	// An object of this type is used to configure desktop
-	// properties like resolution and colors
+	//! An object of this type is used to configure desktop
+	//! properties like resolution and colors
 	// ----
 	if ( (! desktop) || (! card) ) {
 		excNullPointerArgument ();
@@ -65,8 +65,8 @@ SaXManipulateDesktop::SaXManipulateDesktop (
 //------------------------------------
 bool SaXManipulateDesktop::selectDesktop (int desktop) {
 	// .../
-	// select the desktop device the changes should be applied to
-	// if the device does not exist <false> is returned
+	//! select the desktop device the changes should be applied to
+	//! if the device does not exist <false> is returned
 	// ----
 	if ((! mDesktop) || (! mCard) || (! mPath)) {
 		return false;
@@ -83,9 +83,9 @@ bool SaXManipulateDesktop::selectDesktop (int desktop) {
 //------------------------------------
 void SaXManipulateDesktop::calculateModelines (bool calc) {
 	// .../
-	// enable/disable the modeline calculation. This is
-	// onyl needed for non VESA modes or modes which are not
-	// part of the internal X-Server mode pool
+	//! enable/disable the modeline calculation. This is
+	//! onyl needed for non VESA modes or modes which are not
+	//! part of the internal X-Server mode pool
 	// ----
 	if ((! mDesktop) || (! mCard) || (! mPath)) {
 		return;
@@ -102,8 +102,8 @@ void SaXManipulateDesktop::calculateModelines (bool calc) {
 //------------------------------------
 void SaXManipulateDesktop::setResolution (int c,int x,int y) {
 	// .../
-	// set resolution <X>x<Y> for colordepth <c>. Using this
-	// member will overwrite the current resolution setting
+	//! set resolution <X>x<Y> for colordepth <c>. Using this
+	//1 member will overwrite the current resolution setting
 	// ----
 	if ((! mDesktop) || (! mCard) || (! mPath)) {
 		return;
@@ -120,8 +120,8 @@ void SaXManipulateDesktop::setResolution (int c,int x,int y) {
 //------------------------------------
 void SaXManipulateDesktop::addResolution (int c,int x,int y) {
 	// .../
-	// add resolution <X>x<Y> for colordepth <c>. Using this
-	// member will add the resolution to the current setting
+	//! add resolution <X>x<Y> for colordepth <c>. Using this
+	//! member will add the resolution to the current setting
 	// ----
 	if ((! mDesktop) || (! mCard) || (! mPath)) {
 		return;
@@ -141,8 +141,8 @@ void SaXManipulateDesktop::addResolution (int c,int x,int y) {
 //------------------------------------
 void SaXManipulateDesktop::removeResolution (int c,int x,int y) {
 	// .../
-	// remove the resolution <X>x<Y> for colordepth <c> from
-	// the current Modes value
+	//! remove the resolution <X>x<Y> for colordepth <c> from
+	//! the current Modes value
 	// ----
 	if ((! mDesktop) || (! mCard) || (! mPath)) {
 		return;
@@ -161,8 +161,8 @@ void SaXManipulateDesktop::removeResolution (int c,int x,int y) {
 //------------------------------------
 void SaXManipulateDesktop::setVirtualResolution (int c,int x,int y) {
 	// .../
-	// set virtual resolution <X>x<Y> for colordepth <c>. This
-	// call will overwrite the current setting
+	//! set virtual resolution <X>x<Y> for colordepth <c>. This
+	//1 call will overwrite the current setting
 	// ----
 	if ((! mDesktop) || (! mCard) || (! mPath)) {
 		return;
@@ -179,8 +179,8 @@ void SaXManipulateDesktop::setVirtualResolution (int c,int x,int y) {
 //------------------------------------
 void SaXManipulateDesktop::removeVirtualResolution (int c) {
 	// .../
-	// remove the virtual resolution <X>x<Y> set for
-	// colordepth <c>.
+	//! remove the virtual resolution <X>x<Y> set for
+	//! colordepth <c>.
 	// ----
 	if ((! mDesktop) || (! mCard) || (! mPath)) {
 		return;
@@ -197,8 +197,8 @@ void SaXManipulateDesktop::removeVirtualResolution (int c) {
 //------------------------------------
 void SaXManipulateDesktop::setColorDepth (int c) {
 	// .../
-	// set the default color depth. Valid values are
-	// 4,8,15,16 and 24
+	//! set the default color depth. Valid values are
+	//! 4,8,15,16 and 24
 	// ----
 	if ((! mDesktop) || (! mCard) || (! mPath)) {
 		return;
@@ -213,8 +213,8 @@ void SaXManipulateDesktop::setColorDepth (int c) {
 //------------------------------------
 bool SaXManipulateDesktop::is3DCard (void) {
 	// .../
-	// check if the card can be used with 3D hardware
-	// acceleration according to the CDB information
+	//! check if the card can be used with 3D hardware
+	//! acceleration according to the CDB information
 	// ----
 	if ((! mDesktop) || (! mCard) || (! mPath)) {
 		return false;
@@ -248,14 +248,14 @@ bool SaXManipulateDesktop::is3DCard (void) {
 //------------------------------------
 bool SaXManipulateDesktop::enable3D (void) {
 	// .../
-	// enable 3D system. This method will include the needed
-	// extension module to activate 3D. It is necessary to restart
-	// the X-Server after the configuration has been written. The
-	// method will take care about the special nvidia setup which
-	// requires the nvidia installer to uninstall -> disable or
-	// install -> enable the 3D subsystem. NOTE: libsax will not
-	// install or uninstall anything but an exception will be throwed
-	// if the binary nvidia driver is not installed
+	//! enable 3D system. This method will include the needed
+	//! extension module to activate 3D. It is necessary to restart
+	//! the X-Server after the configuration has been written. The
+	//! method will take care about the special nvidia setup which
+	//! requires the nvidia installer to uninstall -> disable or
+	//! install -> enable the 3D subsystem. NOTE: libsax will not
+	//! install or uninstall anything but an exception will be throwed
+	//! if the binary nvidia driver is not installed
 	// ----
 	if ((! mDesktop) || (! mCard) || (! mPath)) {
 		return false;
@@ -342,11 +342,11 @@ bool SaXManipulateDesktop::enable3D (void) {
 //------------------------------------
 bool SaXManipulateDesktop::disable3D (void) {
 	// ....
-	// remove the needed extension module(s) related to the
-	// 3D subsystem. This method will handle the special nvidia
-	// case as well. An exception will be throwed if the nvidia
-	// driver is installed and needs to get uninstalled to
-	// deactivate 3D in this case
+	//! remove the needed extension module(s) related to the
+	//! 3D subsystem. This method will handle the special nvidia
+	//! case as well. An exception will be throwed if the nvidia
+	//! driver is installed and needs to get uninstalled to
+	//! deactivate 3D in this case
 	// ----
 	if ((! mDesktop) || (! mCard) || (! mPath)) {
 		return false;
@@ -433,8 +433,8 @@ bool SaXManipulateDesktop::disable3D (void) {
 //------------------------------------
 void SaXManipulateDesktop::setDisplaySize (int width,int height) {
 	// .../
-	// set the DisplaySize in [mm] for the width and height
-	// of the currently selected display
+	//! set the DisplaySize in [mm] for the width and height
+	//! of the currently selected display
 	// ----
 	if ((! mDesktop) || (! mCard) || (! mPath)) {
 		return;
@@ -449,8 +449,8 @@ void SaXManipulateDesktop::setDisplaySize (int width,int height) {
 //------------------------------------
 void SaXManipulateDesktop::setHsyncRange (double hsmin,double hsmax) {
 	// .../
-	// set the horizontal sync range in [Khz] of the currently
-	// selected display.
+	//! set the horizontal sync range in [Khz] of the currently
+	//! selected display.
 	// ----
 	if ((! mDesktop) || (! mCard) || (! mPath)) {
 		return;
@@ -465,8 +465,8 @@ void SaXManipulateDesktop::setHsyncRange (double hsmin,double hsmax) {
 //------------------------------------
 void SaXManipulateDesktop::setVsyncRange (double vsmin,double vsmax) {
 	// .../
-	// set the vertical sync range in [Hz] of the currently
-	// selected display.
+	//! set the vertical sync range in [Hz] of the currently
+	//! selected display.
 	// ----
 	if ((! mDesktop) || (! mCard) || (! mPath)) {
 		return;
@@ -481,9 +481,9 @@ void SaXManipulateDesktop::setVsyncRange (double vsmin,double vsmax) {
 //------------------------------------
 void SaXManipulateDesktop::enableDPMS (void) {
 	// .../
-	// enable monitor option DPMS, currently there is only
-	// one monitor option so we will set the value and overwrite
-	// the current setting
+	//! enable monitor option DPMS, currently there is only
+	//! one monitor option so we will set the value and overwrite
+	//! the current setting
 	// ----
 	if ((! mDesktop) || (! mCard) || (! mPath)) {
 		return;
@@ -496,8 +496,8 @@ void SaXManipulateDesktop::enableDPMS (void) {
 //------------------------------------
 void SaXManipulateDesktop::disableDPMS (void) {
 	// .../
-	// disable monitor option DPMS by removing the complete
-	// entry. This works because there is only one option available
+	//! disable monitor option DPMS by removing the complete
+	//! entry. This works because there is only one option available
 	// ----
 	if ((! mDesktop) || (! mCard) || (! mPath)) {
 		return;
@@ -510,8 +510,8 @@ void SaXManipulateDesktop::disableDPMS (void) {
 //------------------------------------
 void SaXManipulateDesktop::setMonitorVendor (const QString& vendor) {
 	// .../
-	// set monitor vendor name. This method will only set
-	// the vendor string to identify the monitor
+	//! set monitor vendor name. This method will only set
+	//! the vendor string to identify the monitor
 	// ----
 	if ((! mDesktop) || (! mCard) || (! mPath)) {
 		return;
@@ -524,8 +524,8 @@ void SaXManipulateDesktop::setMonitorVendor (const QString& vendor) {
 //------------------------------------
 void SaXManipulateDesktop::setMonitorName (const QString& name) {
 	// .../
-	// set the monitor model name. This method will only set
-	// the model string to identify the monitor
+	//! set the monitor model name. This method will only set
+	//! the model string to identify the monitor
 	// ----
 	if ((! mDesktop) || (! mCard) || (! mPath)) {
 		return;
@@ -538,10 +538,10 @@ void SaXManipulateDesktop::setMonitorName (const QString& name) {
 //------------------------------------
 bool SaXManipulateDesktop::is3DEnabled (void) {
 	// .../
-	// check if 3D is enabled. first we will have a look at
-	// the driver used. In case of nvidia 3D is always enabled
-	// in any other case the dri module must be used to enable
-	// the 3D subsystem
+	//! check if 3D is enabled. first we will have a look at
+	//! the driver used. In case of nvidia 3D is always enabled
+	//! in any other case the dri module must be used to enable
+	//! the 3D subsystem
 	// ----
 	if ((! mDesktop) || (! mCard) || (! mPath)) {
 		return false;
@@ -566,9 +566,9 @@ bool SaXManipulateDesktop::is3DEnabled (void) {
 //------------------------------------
 QList<QString> SaXManipulateDesktop::getResolutions (int color) {
 	// .../
-	// return a list of resolutions refering to the given
-	// color depth. If there is no resolution defined for the given
-	// color an empty list is returned
+	//! return a list of resolutions refering to the given
+	//! color depth. If there is no resolution defined for the given
+	//! color an empty list is returned
 	// ----
 	if ((! mDesktop) || (! mCard) || (! mPath)) {
 		return QList<QString>();
@@ -593,11 +593,11 @@ QList<QString> SaXManipulateDesktop::getResolutions (int color) {
 //------------------------------------
 QList<QString> SaXManipulateDesktop::getDisplaySize (void) {
 	// .../
-	// get the values for the display size wheras the
-	// first entry in the list refers to the X value in mm
-	// and the second value refers to the Y value in mm. If
-	// there is no display size defined an empty list is
-	// returned
+	//! get the values for the display size wheras the
+	//! first entry in the list refers to the X value in mm
+	//! and the second value refers to the Y value in mm. If
+	//! there is no display size defined an empty list is
+	//! returned
 	// ----
 	if ((! mDesktop) || (! mCard) || (! mPath)) {
 		return QList<QString>();
@@ -622,9 +622,9 @@ QList<QString> SaXManipulateDesktop::getDisplaySize (void) {
 //------------------------------------
 QList<QString> SaXManipulateDesktop::getHsyncRange (void) {
 	// .../
-	// get the values for the horizontal sync range in Khz
-	// the first entry specify the start value and the second
-	// value specify the end of the range
+	//! get the values for the horizontal sync range in Khz
+	//! the first entry specify the start value and the second
+	//! value specify the end of the range
 	// ----
 	if ((! mDesktop) || (! mCard) || (! mPath)) {
 		return QList<QString>();
@@ -649,9 +649,9 @@ QList<QString> SaXManipulateDesktop::getHsyncRange (void) {
 //------------------------------------
 QList<QString> SaXManipulateDesktop::getVsyncRange (void) {
 	// .../
-	// get the values for the vertical sync range in Khz
-	// the first entry specify the start value and the second
-	// value specify the end of the range
+	//! get the values for the vertical sync range in Khz
+	//! the first entry specify the start value and the second
+	//! value specify the end of the range
 	// ----
 	if ((! mDesktop) || (! mCard) || (! mPath)) {
 		return QList<QString>();
@@ -676,9 +676,9 @@ QList<QString> SaXManipulateDesktop::getVsyncRange (void) {
 //------------------------------------
 bool SaXManipulateDesktop::DPMSEnabled (void) {
 	// .../
-	// check if DPMS is enabled by checking the value for
-	// MonitorOptions. Currently there is only one monitor
-	// option available so the check is rather simple
+	//! check if DPMS is enabled by checking the value for
+	//! MonitorOptions. Currently there is only one monitor
+	//! option available so the check is rather simple
 	// ----
 	if ((! mDesktop) || (! mCard) || (! mPath)) {
 		return false;
@@ -695,8 +695,8 @@ bool SaXManipulateDesktop::DPMSEnabled (void) {
 //------------------------------------
 QString SaXManipulateDesktop::getMonitorVendor (void) {
 	// .../
-	// get the name of the monitor vendor for the
-	// currently selected desktop
+	//! get the name of the monitor vendor for the
+	//! currently selected desktop
 	// ----
 	if ((! mDesktop) || (! mCard) || (! mPath)) {
 		return QString();
@@ -709,8 +709,8 @@ QString SaXManipulateDesktop::getMonitorVendor (void) {
 //------------------------------------
 QString SaXManipulateDesktop::getMonitorName (void) {
 	// .../
-	// get the name of the monitor model for the
-	// currently selected desktop
+	//! get the name of the monitor model for the
+	//! currently selected desktop
 	// ----
 	if ((! mDesktop) || (! mCard) || (! mPath)) {
 		return QString();
@@ -723,8 +723,8 @@ QString SaXManipulateDesktop::getMonitorName (void) {
 //------------------------------------
 QString SaXManipulateDesktop::getColorDepth (void) {
 	// .../
-	// get the currently set default color depth for
-	// the active desktop
+	//! get the currently set default color depth for
+	//! the active desktop
 	// ----
 	if ((! mDesktop) || (! mCard) || (! mPath)) {
 		return QString();
@@ -737,8 +737,8 @@ QString SaXManipulateDesktop::getColorDepth (void) {
 //------------------------------------
 bool SaXManipulateDesktop::willCalculateModelines (void) {
 	// .../
-	// check if the modeline calculation is enabled or not
-	// The method will return true if enabled otherwise false
+	//! check if the modeline calculation is enabled or not
+	//! The method will return true if enabled otherwise false
 	// ----
 	if ((! mDesktop) || (! mCard) || (! mPath)) {
 		return false;
@@ -755,9 +755,9 @@ bool SaXManipulateDesktop::willCalculateModelines (void) {
 //------------------------------------
 QString SaXManipulateDesktop::getVirtualResolution (int color) {
 	// .../
-	// get the virtual resolution if defined for the given
-	// color depth. If there is no virtual resolution a (null)
-	// string is returned
+	//! get the virtual resolution if defined for the given
+	//! color depth. If there is no virtual resolution a (null)
+	//! string is returned
 	// ----
 	if ((! mDesktop) || (! mCard) || (! mPath)) {
 		return QString();
@@ -779,9 +779,9 @@ QString SaXManipulateDesktop::getVirtualResolution (int color) {
 //------------------------------------
 void SaXManipulateDesktop::setCDBMonitor ( const QString& group ) {
 	// .../
-	// set all monitor data associated with the given group name to
-	// the current desktop data. The group name consists of the
-	// vendor and model name separated by a colon
+	//! set all monitor data associated with the given group name to
+	//! the current desktop data. The group name consists of the
+	//! vendor and model name separated by a colon
 	// ----
 	if ( ! mCDBMonitors ) {
 		mCDBMonitors = new SaXProcess ();
@@ -807,9 +807,9 @@ void SaXManipulateDesktop::setCDBMonitor ( const QString& group ) {
 //------------------------------------
 QList<QString> SaXManipulateDesktop::getCDBMonitorList ( void ) {
 	// .../
-	// retrieve a list of supported monitor groups. Each item
-	// contains the vendor and the model in its name separated
-	// by a colon
+	//! retrieve a list of supported monitor groups. Each item
+	//1 contains the vendor and the model in its name separated
+	//! by a colon
 	// ----
 	mCDBMonitorList.clear();
 	if ( ! mCDBMonitors ) {
--- sax-head/libsax/desktop.h
+++ sax-head/libsax/desktop.h
@@ -87,6 +87,60 @@ class SaXManipulateDesktopIF : public SaXException {
 //====================================
 // Class SaXManipulateDesktop...
 //------------------------------------
+/*! \brief SaX2 -  Desktop manipulator class.
+*
+* The desktop manipulator requires three import objects to become
+* created:
+*
+* - Desktop
+* - Card
+* - Path
+*
+* Once created the manipulator object is able to get/set desktop related
+* information like resolutions color depth monitor specs or 3D. The
+* following example shows how to use the desktop manipulator for adding
+* a new standard resolution in 24 bit color depth:
+*
+* \code
+* #include <sax.h>
+*
+* int main (void) {
+*     SaXException().setDebug (true);
+*     QDict<SaXImport> section;
+*     int importID[] = {
+*         SAX_CARD,
+*         SAX_DESKTOP,
+*         SAX_PATH,
+*     };
+*     printf ("Importing data...\n");
+*     SaXConfig* config = new SaXConfig;
+*     for (int id=0; id<3; id++) {
+*         SaXImport* import = new SaXImport ( importID[id] );
+*         import -> setSource ( SAX_SYSTEM_CONFIG );
+*         import -> doImport();
+*         config -> addImport (import);
+*         section.insert (
+*             import->getSectionName(),import
+*         );
+*     }
+*     printf ("Setting up resolution...\n");
+*     SaXManipulateDesktop mDesktop (
+*         section["Desktop"],section["Card"],section["Path"]
+*     );
+*     if (mDesktop.selectDesktop (0)) {
+*         mDesktop.addResolution (24,1600,1200);
+*     }
+*     printf ("Writing configuration\n");
+*     config -> setMode (SAX_MERGE);
+*     if ( ! config -> createConfiguration() ) {
+*         printf ("%s\n",config->errorString());
+*         printf ("%s\n",config->getParseErrorValue());
+*         return 1;
+*     }
+*     return 0;
+* }
+* \endcode
+*/
 class SaXManipulateDesktop : public SaXManipulateDesktopIF {
 	private:
 	SaXImport*     mDesktop;
