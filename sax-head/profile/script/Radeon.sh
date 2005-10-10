db82c1166caf8ab888a8ebbe41d5af38a5b218ab
ms <ms@a26604f9-b0b9-0310-ba11-92d0f7005c15>
2005-10-10 14:05:16 +0000

    - added support for automatic configuration of ATI binary driver (fglrx)

---
+++ sax-head/profile/script/Radeon.sh
@@ -0,0 +1,22 @@
+#====================================
+# Init profile script
+#------------------------------------
+profile=$(basename $0 | cut -f1 -d.)
+profile="/usr/share/sax/profile/$profile"
+if [ ! -f "$profile" ] || [ "$UID" -ne 0 ];then
+	exit 0
+fi
+
+#====================================
+# Copy profile
+#------------------------------------
+cp $profile $profile.tmp
+profile="$profile.tmp"
+
+#====================================
+# Check if profile is valid
+#------------------------------------
+fglrx=/usr/share/sax/profile/FireGL
+if [ -f /usr/X11R6/lib/modules/drivers/fglrx_drv.so ];then
+	cp $fglrx $profile
+fi
