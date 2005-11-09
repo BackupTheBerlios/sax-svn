da530062a811c51e78d517fc71f08e1df3f85222
marcus_schaefer <marcus_schaefer@a26604f9-b0b9-0310-ba11-92d0f7005c15>
2005-11-09 16:57:40 +0000

    - update monitor database (#132640) (#131959) - fixed typo in startup/sax.sh and sax-man/sax2 (#132882)

--- sax-head/startup/sax.sh
+++ sax-head/startup/sax.sh
@@ -104,7 +104,7 @@ function usage() {
 	echo "  Activate the SaX2 batch modus. If no filename is given an"
 	echo "  interactive shell is started to set/overwrite configuration"
 	echo "  parameters. If the filename doesn't contain any / characters"
-	echo "  it is asumed to be found in /usr/share/sax/profile"
+	echo "  it is assumed to be found in /usr/share/sax/profile"
 	echo
 	echo "[ -a | --auto ]"
 	echo "  enable automatic configuration." 
