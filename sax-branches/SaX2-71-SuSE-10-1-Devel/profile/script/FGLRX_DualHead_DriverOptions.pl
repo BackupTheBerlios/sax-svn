212d178df97cb268aee466e5c2a71b1cf29b5ab2
marcus_schaefer <marcus_schaefer@a26604f9-b0b9-0310-ba11-92d0f7005c15>
2006-04-10 10:38:18 +0000

    - fixed quoting bug for secondary monitor name on laptops

--- sax-branches/SaX2-71-SuSE-10-1-Devel/profile/script/FGLRX_DualHead_DriverOptions.pl
+++ sax-branches/SaX2-71-SuSE-10-1-Devel/profile/script/FGLRX_DualHead_DriverOptions.pl
@@ -50,7 +50,7 @@ if (ProfileIsNoteBookHardware()) {
 	$dx = eval $dt;
 	print FD "$dx=\"SaXDualMonitorVendor\" \"__VESA__\"\n";
 	$dx = eval $dt;
-	print FD "$dx=\"SaXDualMonitorModel\" \"1024X768@60HZ\"\n";
+	print FD "$dx=\"SaXDualMonitorModel\" \"1024X768\@60HZ\"\n";
 	close FD;
 } else {
 	#====================================
