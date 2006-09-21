e31b238ad70042e2d3e45ea4dcd4f3c6b393a699
marcus_schaefer <marcus_schaefer@a26604f9-b0b9-0310-ba11-92d0f7005c15>
2006-09-21 12:50:17 +0000

    - added support for patching bios using a white list of   devices. The IntelPatch.map contains the command and   the parameters to patch the bios (#201338)

--- sax-head/libsax/tools/createAPI.pl
+++ sax-head/libsax/tools/createAPI.pl
@@ -108,6 +108,63 @@ sub createXOrgConfig {
 	}
 }
 
+#=====================================
+# getIntelPatchMap
+#-------------------------------------
+sub getIntelPatchMap {
+	my $resx = shift;
+	my $resy = shift;
+	my %result;
+	if (! open (FD,"/usr/share/sax/sysp/maps/IntelPatch.map")) {
+		return undef;
+	}
+	foreach my $line (<FD>) {
+		if ($line =~ /^#/) {
+			next;
+		}
+		if ($line =~ /(.*):(.*):(.*)/) {
+			my $code = $1;
+			my $args = $2;
+			my $prog = $3;
+			$code =~ s/^ +//; $code =~ s/ +$//;
+			$args =~ s/^ +//; $args =~ s/ +$//;
+			$prog =~ s/^ +//; $prog =~ s/ +$//;
+			if ((defined $resx) && (defined $resy)) {
+				$args =~ s/\%x/$resx/;
+				$args =~ s/\%y/$resy/;
+			}
+			$result{$code}{$prog} = $args;
+		}
+	}
+	close  FD;
+	return %result;
+}
+
+#=====================================
+# getIntelPatchCode
+#-------------------------------------
+sub getIntelPatchCode {
+	my @data = qx (/usr/sbin/sysp -q server);
+	my $subvendor = 0;
+	my $subdevice = 0;
+	my $vendor    = 0;
+	my $device    = 0;
+	foreach my $item (@data) {
+		if ($item =~ /VID.*: 0x(.*)/) {
+			$vendor = $1;
+		}
+		if ($item =~ /DID.*: 0x(.*)/) {
+			$device = $1;
+		}
+		if ($item =~ /SUB-VID.*: 0x(.*)/) {
+			$subvendor = $1;
+		}
+		if ($item =~ /SUB-DID.*: 0x(.*)/) {
+			$subdevice = $1;
+		}
+	}
+	return "0x$vendor$device$subvendor$subdevice";
+}
 
 #=====================================
 # Run... 
@@ -116,21 +173,30 @@ init();
 my $patchBIOS = createXOrgConfig();
 if ($patchBIOS ne "ok") {
 	#=====================================
-	# patching bios using 855resolution
+	# i810 device: check for patching bios
 	#-------------------------------------
-	my $tool = "/usr/sbin/855resolution";
-	if ( -f $tool ) {
-		my @xy = split (/x/,$patchBIOS);
-		my $result = qx ($tool 3c $xy[0] $xy[1] 2>&1);
-		my $args = "3c $xy[0] $xy[1]";
-		my $key  = "VIDEOBIOS_PARAMETERS";
-		my $file = "/etc/sysconfig/videobios";
-		if (-f $file) {
-			qx (cat $file | sed -e s!$key=.*!$key=\\""$args"\\"! > $file.tmp);
-			qx (mv $file.tmp $file);
-			$key = "VIDEOBIOS_PATCH";
-			qx (cat $file | sed -e s!$key=.*!$key=\\""yes"\\"! > $file.tmp);
-			qx (mv $file.tmp $file);
+	my @xy = split (/x/,$patchBIOS);
+	my %patch = getIntelPatchMap  ($xy[0],$xy[1]);
+	my $icode = getIntelPatchCode ();
+	foreach my $code (keys %patch) {
+		if ($code ne $icode) {
+			next
+		}
+		foreach my $prog (keys %{$patch{$code}}) {
+		if (-f $prog) {
+			my $args = $patch{$code}{$prog};
+			my $result = qx ($prog $args 2>&1);
+			my $key  = "VIDEOBIOS_PARAMETERS";
+			my $file = "/etc/sysconfig/videobios";
+			my $tmpf = "$file.tmp";
+			if (-f $file) {
+				qx (cat $file | sed -e s!$key=.*!$key=\\""$args"\\"! > $tmpf);
+				qx (mv $tmpf $file);
+				$key = "VIDEOBIOS_PATCH";
+				qx (cat $file | sed -e s!$key=.*!$key=\\""yes"\\"! > $tmpf);
+				qx (mv $tmpf $file);
+			}
+		}
 		}
 	}
 }
