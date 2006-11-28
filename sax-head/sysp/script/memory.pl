696dc3a1eed1e250edfae7870468c942c9b57bc9
marcus_schaefer <marcus_schaefer@a26604f9-b0b9-0310-ba11-92d0f7005c15>
2006-11-28 09:24:40 +0000

    - deactivate memory detection, workaround for bug (#220197) - update i810 patch map for Thinpad R52 and GW MX6923b (#223745) - deactivate HorizScrollDelta in synaptics driver (#223827)

--- sax-head/sysp/script/memory.pl
+++ sax-head/sysp/script/memory.pl
@@ -14,6 +14,21 @@
 use strict;
 use Env;
 
+# /.../
+# Workaround for Bug: #220197
+# Unfortunately X11 doesn't provide a clean interface to detect
+# the amount of videoram installed on the graphics card. According
+# to this it is needed to initialize the X-Server and check the log
+# to find out that value. The following workaround will switch off
+# the memory detection because there are bad side effects while
+# calling X11 with the option -probeonly. The reason for this
+# workaround is the broken X11 implementation and I'm really hoping
+# this will be fixed as soon as possible
+# ----
+print "0\n";
+exit 0;
+# ----
+
 my $param = "-probeonly -logverbose 255 -verbose -xf86config $ARGV[0]";
 my $video = qx (X $param :99 2>&1 | grep -i videoram);
 if ($video =~ /(\d+) kByte/) {
