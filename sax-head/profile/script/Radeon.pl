ad2808400d7685e020ece26904ba01a8594796f7
sndirsch <sndirsch@a26604f9-b0b9-0310-ba11-92d0f7005c15>
2008-08-27 01:51:08 +0000

    - the logic in existing dynamic Radeon.pl profile was obsolete since   a long time - AFAIK; replaced with the logic also used by Intel.pl,   RadeonHD.pl, which make sure that 'Option "monitor-<output>"' is   used (bnc #420124)

--- sax-head/profile/script/Radeon.pl
+++ sax-head/profile/script/Radeon.pl
@@ -10,10 +10,39 @@ use Profile;
 #------------------------------------
 my $profile = ProfileInitScript();
 
+#=====================================
+# get SYSP xstuff data
+#-------------------------------------
+my $outputName;
+my $xstuff = new SaX::SaXImportSysp ($SaX::SYSP_DESKTOP);
+$xstuff->doImport();
+my $rroutput = $xstuff->getItem("RandR");
+my $randrok  = 1;
+if ((! defined $rroutput) || ($rroutput eq "<undefined>")) {
+	$randrok = 0;
+}
+if ($randrok) {
+	#=====================================
+	# use first randr plugin as primary
+	#-------------------------------------
+	my @channels = split ("%",$rroutput);
+	foreach my $channel (@channels) {
+		my @data = split (" ",$channel);
+		my $outputState = $data[1];
+		if ($outputState ne "connected") {
+			next;
+		}
+		$outputName = $data[0];
+		last;
+	}
+}
 #====================================
 # Do the profile adaptions...
 #------------------------------------
-my $fglrx="/usr/share/sax/profile/FireGL";
-if (! ProfileIsXOrgVendor ("fglrx")) {
-	qx (cp $fglrx $profile);
+open (FD,">",$profile) ||
+	die "Radeon: Can't open $profile: $!";
+if (defined $outputName) {
+	print FD "Device -> [X] -> Raw -> 18 -> Option = ";
+	print FD "\"monitor-$outputName\" \"Monitor[0]\"\n";
 }
+close FD;
